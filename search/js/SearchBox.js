c331046a4f16d100d8874ad39a5013e214b2cf26
serp-ya <sergei.kundryukov1989@gmail.com>
2018-04-13 01:30:05 +0300

    Реализация плавающей строки

--- search/js/SearchBox.js
+++ search/js/SearchBox.js
@@ -3,17 +3,40 @@ class SearchBox extends React.Component {
   constructor(props) {
     super(props);
     this.state = { fixed: false };
+
+    this.searchBar = null;
+    this.getSearchBar = searchBar => this.searchBar = searchBar;
   }
+
+
+  componentDidMount() {
+    document.addEventListener('scroll', this.setPosition);
+  }
+
+  
+  componentWillUnmount() {
+    document.removeEventListener('scroll', this.setPosition);
+  }
+  
 
   render() {
-    return <SearchBoxView fixed={this.state.fixed} />
+    return <SearchBoxView refs={{getSearchBar: this.getSearchBar}} fixed={this.state.fixed} />
   }
+
 
   isFixed() {
-    return undefined;
+    const offsetTopSearchBar = this.searchBar.offsetTop;
+    const offsetTopWindow = window.pageYOffset;
+    
+    return offsetTopSearchBar < offsetTopWindow;
   }
 
-  setPosition() {
-    return undefined;
+  setPosition = () => {
+    const currentState = this.state.fixed;
+    const newState = this.isFixed();
+
+    if (currentState !== newState) {
+      this.setState({fixed: newState})
+    }
   }
 }
