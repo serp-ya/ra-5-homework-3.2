388a0bdeba217753b8a501c60bcdfa838d8e3483
serp-ya <sergei.kundryukov1989@gmail.com>
2018-04-15 17:47:59 +0300

    Корзина готова

--- shop/js/Cart.js
+++ shop/js/Cart.js
@@ -1,5 +1,17 @@
 class Cart extends React.Component {
 
+  shouldComponentUpdate(nextProps) {
+    const prevIsOpen = this.props.isOpen;
+    const prevItemsLength = this.props.items.length;
+
+    const {
+      isOpen: nextIsOpen,
+      items: nextItems
+    } = nextProps;
+
+    return (prevIsOpen !== nextIsOpen) || (nextIsOpen && (prevItemsLength !== nextItems.length));
+  }
+
   render() {
     return (
       <CartView {...this.props} />
